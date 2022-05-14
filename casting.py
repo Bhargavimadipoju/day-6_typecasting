4fb155879530b042f965be50c4b56b1a5e17c24e
Bhargavimadipoju <104247048+Bhargavimadipoju@users.noreply.github.com>
2022-05-14 18:24:43 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,26 @@
+""" DATE: 14-MAY-2022
+NAME:BHARGAVIMADIPOJU
+FILE DESC : TYPE CONVERSION AND CASTING """
+a=3
+# print(float(a)) #3.0
+# print('%f' %a) #3.000000
+# print(str(a)) #3
+# print('%S'%a)
+
+b=4.78
+# print(type(b)) #float
+# print(float(b)) #4.78
+# print(int(b)) #4
+# print('%f'%b) #4.780000
+# print('%s'%b) #4.78
+# print(str(b)) #4.7
+
+c='bhargavi'
+# print(int(c)) #value error
+# print(float(c)) #value error
+
+d='9'
+# print(int(d)) #9
+# print(float(d)) #9.0
+# print('%d'%d) #type error
+# print('%f'%d) #type error
